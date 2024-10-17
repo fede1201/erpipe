7402e96a020caa67b767b22602415d52865d74ab
Federico Luzi <fluzi@e3r5p1.42roma.it>
2024-10-17 17:54:50 +0200

    quasi def(no norminette)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fluzi <fluzi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:45:52 by fluzi             #+#    #+#             */
-/*   Updated: 2024/10/14 16:14:10 by fluzi            ###   ########.fr       */
+/*   Updated: 2024/10/17 17:51:31 by fluzi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,6 @@ int	free_matrix(char **matrix)
 	return (1);
 }
 
-
-void	stampmat(char **mtr)
-{
-	int	i;
-
-	i = 0;
-	while (mtr[i])
-	{
-		printf("$(red)%s \n", mtr[i]);
-		i++;
-	}
-	printf("\n\n");
-}
-// Funzione per trovare la variabile PATH dall'ambiente
 char	*get_line_path(char **envp)
 {
 	int	i;
@@ -49,148 +35,161 @@ char	*get_line_path(char **envp)
 	{
 		if (envp[i][0] == 'P' && envp[i][1] == 'A' &&
 			envp[i][2] == 'T' && envp[i][3] == 'H' && envp[i][4] == '=')
-		{
-			printf("PATH trovata: %s\n", envp[i]); 
 			return (envp[i]);
-		}
 		i++;
 	}
-	return (NULL);
+	exit(1);
 }
 
-
-// Funzione per trovare il percorso del comando
-char	*find_path(char *fun, char **envp)
+char	*path_utils(char *path, char *fun, int i, int type)
 {
-	char	**split_path;
-	char	*path;
 	char	*p1;
 	char	*p2;
-	int		i;
 	char	**fun_split;
+	char	**split_path;
 
-	path = get_line_path(envp);
-	if (!path)
-	{
-		perror("PATH NON TROVATO!");
-		exit(EXIT_FAILURE);
-	}
-	split_path = ft_split(path + 5, ':'); 
+	split_path = ft_split(path + 5, ':');
 	fun_split = ft_split(fun, ' ');
 	i = 0;
-	stampmat(split_path);
-	
-	
 	while (split_path[i])
 	{
 		p1 = ft_strjoin(split_path[i], "/");
 		p2 = ft_strjoin(p1, fun_split[0]);
-		printf("PROVA: %s\n", p2);
 		free(p1);
 		if (access(p2, X_OK) == 0)
 		{
+			free(fun_split);
 			free_matrix(split_path);
-			printf("\n%s\n\n", p2);
-			return (p2); 
+			return (p2);
 		}
 		free(p2);
 		i++;
 	}
-	free_matrix(split_path); 
-	return (NULL); 
+	free(fun_split);
+	free_matrix(split_path);
+	if (type == 1)
+		exit(127);
+	exit(0);
 }
 
-// Funzione che esegue il comando
-void	exe_func(char *argv, char **envp)
+char	*find_path(char *fun, char **envp, int type)
+{
+	char	*path;
+
+	path = get_line_path(envp);
+	if (!path)
+	{
+		perror("Error \n");
+		exit(0);
+	}
+	return (path_utils(path, fun, 0, type));
+}
+
+void	exe_func(char *argv, char **envp, int type)
 {
 	char	**matrix_argv;
 	char	*path;
 
-	path = find_path(argv, envp);
+	path = find_path(argv, envp, type);
 	if (!path)
 	{
-		perror("Comando non trovato");
-		exit(EXIT_FAILURE);
+		perror("Command not found\n");
+		exit(0);
 	}
 	matrix_argv = ft_split(argv, ' ');
 	if (execve(path, matrix_argv, envp) == -1)
 	{
-		perror("Errore nell'esecuzione del comando");
+		perror("Error \n");
 		free(path);
-		free_matrix(matrix_argv); 
-		exit(EXIT_FAILURE);
+		free_matrix(matrix_argv);
+		if (type == 0)
+			exit(0);
+		exit(127);
 	}
-	free(path); 
-	free_matrix(matrix_argv); 
+	free(path);
+	free_matrix(matrix_argv);
 }
 
-// Funzione che gestisce la logica di fork e pipe
 void	fork_t(char **argv, int *fd, char **envp, int t)
 {
 	int	nfd;
 
-	if (t == 0) 
+	if (t == 0)
 	{
 		nfd = open(argv[1], O_RDONLY);
 		if (nfd < 0)
 		{
-			perror("Errore nell'apertura del file di input");
-			exit(EXIT_FAILURE);
+			perror("Error \n");
+			exit(EXIT_SUCCESS);
 		}
-		dup2(nfd, STDIN_FILENO); 
-		dup2(fd[1], STDOUT_FILENO); 
+		dup2(nfd, STDIN_FILENO);
+		dup2(fd[1], STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
-		exe_func(argv[2], envp); 
-		close(nfd); 
+		exe_func(argv[2], envp, 0);
+		close(nfd);
 	}
-	else 
+	else
 	{
 		nfd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 		if (nfd < 0)
 		{
-			perror("Errore nell'apertura del file di output");
+			perror("Error \n");
 			exit(EXIT_FAILURE);
 		}
-		dup2(fd[0], STDIN_FILENO); 
-		dup2(nfd, STDOUT_FILENO); 
-		close(fd[0]);
+		dup2(fd[0], STDIN_FILENO);
+		dup2(nfd, STDOUT_FILENO);
 		close(fd[1]);
-		exe_func(argv[3], envp); 
-		close(nfd); 
+		exe_func(argv[3], envp, 1);
+		close(nfd);
 	}
 }
 
-// Funzione principale
 int	main(int argc, char **argv, char **envp)
 {
 	int		fd[2];
-	pid_t	pid;
-
+	pid_t	pid1, pid2;
+	int		status1, status2;
 
 	if (argc != 5)
 	{
-		perror("Numero di argomenti non valido");
+		perror("Error \n");
 		exit(EXIT_FAILURE);
 	}
-	
 	if (pipe(fd) == -1)
 	{
-		perror("Errore nella creazione della pipe");
+		perror("Error \n");
 		exit(EXIT_FAILURE);
 	}
-	pid = fork(); 
-	if (pid < 0)
+	pid1 = fork();
+	if (pid1 < 0)
 	{
-		perror("Errore nel fork");
+		perror("Error \n");
 		exit(EXIT_FAILURE);
 	}
-	else if (pid == 0) 
+	else if (pid1 == 0)
 	{
 		fork_t(argv, fd, envp, 0);
-		exit(EXIT_SUCCESS);
+		exit(1);
 	}
-	waitpid(pid, NULL, 0); 
-	fork_t(argv, fd, envp, 1); 
+	pid2 = fork();
+	if (pid2 < 0)
+	{
+		perror("Error \n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid2 == 0)
+	{
+		fork_t(argv, fd, envp, 1);
+		exit(1);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, &status1, 0);
+	waitpid(pid2, &status2, 0);
+	if (WIFEXITED(status1) && WEXITSTATUS(status1) != 0)
+		exit(WEXITSTATUS(status1));
+	if (WIFEXITED(status2) && WEXITSTATUS(status2) != 0)
+		exit(WEXITSTATUS(status2));
 	return (0);
 }
